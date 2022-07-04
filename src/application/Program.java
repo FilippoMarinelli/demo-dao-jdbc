c8e822a64e670998f3d4365060b2c5af3cdfd936
FilippoMarinelli <lipo-x123@outlook.com>
2022-07-04 11:12:34 -0300

    Find by ID Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,17 @@
 package application;
 
+import entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 
 	}
 
